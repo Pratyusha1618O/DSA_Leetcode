72ca7d0695b116cc5211463a464b32cd62116c68
Pratyusha Bhandary <128484889+Pratyusha1618O@users.noreply.github.com>
2026-09-02 09:18:06 +0530

    Time: 1 ms (31.89%) | Memory: 44.8 MB (94.09%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,13 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        // int nums2[] = new []int;
-        // for (int i=0; i<nums1.length; i++){
-        //     for(int j=1; j<nums1.length; j++){
-        //         nums2[i] = nums1[i];
-
-
-        //     }
-        // }
         return true;
     }
 }
