39903501bfa4053ed844a2db02fa8e274aba7b59
Pratyusha Bhandary <128484889+Pratyusha1618O@users.noreply.github.com>
2026-09-04 09:31:44 +0530

    Time: 1507 ms (4.47%) | Memory: 46.9 MB (15.74%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        
+        int max = nums[0];
+        int min = nums[0];
+
+        for(int i=0; i<nums.length; i++){
+            
+            for(int j=i; j<nums.length; j++){
+                int[] subArray1 = Arrays.copyOfRange(nums, i, nums.length);
+                Arrays.toString(subArray1);
+                min = Arrays.stream(subArray1).min().getAsInt();
+            }
+            
+            if(i == 0){
+                max = nums[0];
+            }
+            else{
+                for(int j=0; j<=i; j++){
+                    int[] subArray2 = Arrays.copyOfRange(nums, 0, i);
+                    Arrays.toString(subArray2);
+                    max = Arrays.stream(subArray2).max().getAsInt();
+                }
+            }
+
+            int score = max - min;
+            if(score <= k){
+                return i;
+            }
+            
+            
+        }
+
+        return -1;
+    }
+}
