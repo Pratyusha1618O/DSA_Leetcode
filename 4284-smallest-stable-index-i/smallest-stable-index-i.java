ed65da0bf747239391569e8a491f9d619833a569
Pratyusha Bhandary <128484889+Pratyusha1618O@users.noreply.github.com>
2026-09-04 09:39:00 +0530

    Time: 15 ms (4.47%) | Memory: 47.2 MB (15.74%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.java
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -6,20 +6,20 @@ class Solution {
 
         for(int i=0; i<nums.length; i++){
             
-            for(int j=i; j<nums.length; j++){
+            // for(int j=i; j<nums.length; j++){
                 int[] subArray1 = Arrays.copyOfRange(nums, i, nums.length);
                 min = Arrays.stream(subArray1).min().getAsInt();
-            }
+            // }
             
-            if(i == 0){
-                max = nums[0];
-            }
-            else{
-                for(int j=0; j<=i; j++){
+            // if(i == 0){
+            //     max = nums[0];
+            // }
+            // else{
+            //     for(int j=0; j<=i; j++){
                     int[] subArray2 = Arrays.copyOfRange(nums, 0, i+1);
                     max = Arrays.stream(subArray2).max().getAsInt();
-                }
-            }
+            //     }
+            // }
 
             int score = max - min;
             if(score <= k){
