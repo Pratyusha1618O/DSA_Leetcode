b082c748ddb42d550d9282f47469037829c26832
Pratyusha Bhandary <128484889+Pratyusha1618O@users.noreply.github.com>
2026-09-06 08:59:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (89.63%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -1,41 +1,39 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         
-        // int index = 0;
-        // int tcount = 0;
+        int index = 0;
+        int tcount = 0;
 
-        // // Arrays.sort(nums);
-
-        // for(int elem : nums){
-        //     if(elem == target){
-        //         tcount++;
-        //     }
-        //     else if(elem < target){
-        //         index++;
-        //     }
-        // }
-
-        // List<Integer> ans = new ArrayList<>();
-
-        // while(tcount > 0){
-        //     ans.add(index);
-        //     index++;
-        //     tcount--;
-        // }
-
-        // return ans;
-
-
-        List<Integer> ans = new ArrayList<>();
-
-        Arrays.sort(nums);
-
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == target){
-                ans.add(i);
+        for(int elem : nums){
+            if(elem == target){
+                tcount++;
+            }
+            else if(elem < target){
+                index++;
             }
         }
 
+        List<Integer> ans = new ArrayList<>();
+
+        while(tcount > 0){
+            ans.add(index);
+            index++;
+            tcount--;
+        }
+
         return ans;
+
+
+        // List<Integer> ans = new ArrayList<>();
+
+        // Arrays.sort(nums);
+
+        // for(int i=0; i<nums.length; i++){
+        //     if(nums[i] == target){
+        //         ans.add(i);
+        //     }
+        // }
+
+        // return ans;
     }
 }
