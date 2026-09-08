df0a011ba8bb8bd74249d011be992d0140ff32e2
Pratyusha Bhandary <128484889+Pratyusha1618O@users.noreply.github.com>
2026-09-08 09:42:17 +0530

    Time: 1 ms (83.65%) | Memory: 42.7 MB (22.48%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,38 +1,41 @@
 class Solution {
     public int countCommas(int n) {
-        
-
-        int count = 0;
-        int copy = n;
-
-        // No of digits
-        while(copy>0){
-            copy = copy/10;
-            count++;
-        }
-
-        if(count <= 3){
+        if(n < 999){
             return 0;
         }
-        else{
-            int nums = 0;
-            int totalComma = 0;
+        return n - 999;
 
-            for(int i=1000; i<=n; i++){
+        // int count = 0;
+        // int copy = n;
 
-                int j = i;
-                int digCount = 0;
-                while(j>0){
-                    j = j / 10;
-                    digCount++;
-                }
-                int comma = (digCount - 1) / 3;
-                totalComma += comma;
+        // // No of digits
+        // while(copy>0){
+        //     copy = copy/10;
+        //     count++;
+        // }
+
+        // if(count <= 3){
+        //     return 0;
+        // }
+        // else{
+        //     int nums = 0;
+        //     int totalComma = 0;
+
+        //     for(int i=1000; i<=n; i++){
+
+        //         int j = i;
+        //         int digCount = 0;
+        //         while(j>0){
+        //             j = j / 10;
+        //             digCount++;
+        //         }
+        //         int comma = (digCount - 1) / 3;
+        //         totalComma += comma;
                 
-            }
+        //     }
 
-            return totalComma;
-        }
+        //     return totalComma;
+        // }
 
 
     }
